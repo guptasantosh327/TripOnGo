54e9cda6e0aa4c32db3269b88054d6b4e3c169b0
Santosh Gupta <guptasantosh327@gmail.com>
2020-08-12 00:14:01 +0530

    Trying to solve thesignup issue

--- controllers/authController.js
+++ controllers/authController.js
@@ -40,7 +40,7 @@ exports.signUp = catchAsync(async (req, res, next) => {
   const url = `${req.protocol}://${req.get('host')}/me`;
   // console.log(url);
   await new Email(newUser, url).sendWelcome();
-  createSendToken(newUser, 201, res);
+  createSendToken(newUser, 201, req, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
